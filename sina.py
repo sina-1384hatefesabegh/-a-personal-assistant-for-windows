8b5e0c91c056d864c4d1ba2fb35e38d94db84ce3
Sajjad Azarmanesh <148378156+sina-1384hatefesabegh@users.noreply.github.com>
2025-05-29 18:19:45 +0330

    2sina.py

--- sina.py
+++ sina.py
@@ -1,2 +1,9 @@
 "@Sajjad_Azarmanesh   در تلگرام پیام دهید"
 """سورس کد سینا به دلیل سوااستفاده های صورت گرفته و استفاده بدون اجازه شرکتی حذف شده"""
+
+"@Sajjad_Azarmanesh send a message on Telegram"
+"""Sina source code has been removed due to misuse and use without company permission"""
+
+
+
+
